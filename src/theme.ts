796d973ca831e695397657386c31fdc317144ae6
baedormann <89854178+baedormann@users.noreply.github.com>
2025-06-19 11:15:22 +0900

    리액트 쿼리 추가, prettier 설정(2), tailwindcss + mui 연동

--- src/theme.ts
+++ src/theme.ts
@@ -1,9 +1,9 @@
-"use client";
-import { createTheme } from "@mui/material/styles";
+'use client';
+import { createTheme } from '@mui/material/styles';
 
 const theme = createTheme({
   typography: {
-    fontFamily: "var(--font-roboto)",
+    fontFamily: 'var(--font-roboto)',
   },
   cssVariables: true,
 });
